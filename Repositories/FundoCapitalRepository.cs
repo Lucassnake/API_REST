5322a84bbfe590ca1a6fc62b6fc370a85d8142e0
Lucas Fonseca <lucasjosefonsecasilva@gmail.com>
2020-10-19 13:59:20 -0300

    update code

--- Repositories/FundoCapitalRepository.cs
+++ Repositories/FundoCapitalRepository.cs
@@ -1,43 +1,54 @@
-﻿using ApiRest.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using ApiRest.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 
-namespace ApiRest.Repositories
-{
-    public class FundoCapitalRepository : IFundoCapitalRepository 
-    {
-        private readonly List<FundoCapital> _storage;
+namespace ApiRest.Repositories {
+    public class FundoCapitalRepository : IFundoCapitalRepository {
+        
+        public IEnumerable<Indicador> Get (
+            [FromServices] IConfiguration config) {
+            using (SqlConnection conexao =
+                new SqlConnection (config.GetConnectionString ("BaseIndicadores"))) {
+                
+                //return conexao.Query<Indicador> ("SELECT * FROM dbo.Indicadores");
+                
 
-        public FundoCapitalRepository()
-        {
-            _storage = new List<FundoCapital>();
+                public FundoCapitalRepository () {
+                    _storage = new List<FundoCapital> ();
+                }
+
+                public void Adicionar (FundoCapital fundo) {
+                    _storage.Add (fundo);
+                }
+
+                public void Alterar (FundoCapital fundo) {
+                    conexao.Query<Indicador> ("UPDATE * FROM dbo.FundoCapital");
+                }
+
+                public IEnumerable<FundoCapital> ListarFundos () {
+                    return conexao.Query<Indicador> ("SELECT * FROM dbo.FundoCapital");
+                }
+
+                public FundoCapital ObeterPorID (Guid id) {
+                    return conexao.Query<Indicador> ("SELECT * FROM dbo.FundoCapital WHERE = {id}")
+                }
+
+                public void RemoverFundo (FundoCapital fundo) {
+                    _storage.Remove (fundo);
+                }
+            }
         }
+    }
 
-        public void Adicionar(FundoCapital fundo)
-        {
-            _storage.Add(fundo);
-        }
-
-        public void Alterar(FundoCapital fundo)
-        {
-            var index = _storage.FindIndex(0, 1, x => x.Id == fundo.Id);
-            _storage[index] = fundo;
-        }
-
-        public IEnumerable<FundoCapital> ListarFundos()
-        {
-            return _storage;
-        }
-
-        public FundoCapital ObeterPorID(Guid id)
-        {
-            return _storage.FirstOrDefault(x => x.Id == id);// Retorna se o ID do elemento for igual o ID passado como parametro.
-        }
-
-        public void RemoverFundo(FundoCapital fundo)
-         {
-            _storage.Remove(fundo);
+    public IEnumerable<Indicador> Get (
+        [FromServices] IConfiguration config) {
+        using (SqlConnection conexao =
+            new SqlConnection (config.GetConnectionString ("FundoCapitalTable"))) {
+            return conexao.Query<Indicador> ("SELECT * FROM dbo.Indicadores");
         }
     }
 }
